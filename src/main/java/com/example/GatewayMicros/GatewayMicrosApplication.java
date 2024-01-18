445f44c3ba99d851686d2dc531f21b4513a3d4d7
f11v <146761375+f11v@users.noreply.github.com>
2024-01-18 11:47:02 -0500

    .

---
+++ src/main/java/com/example/GatewayMicros/GatewayMicrosApplication.java
@@ -0,0 +1,13 @@
+package com.example.GatewayMicros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayMicrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayMicrosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/GatewayMicros/GatewayMicrosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GatewayMicros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayMicrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
