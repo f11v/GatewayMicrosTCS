a9788876fe3a43a1509dbab8f3bdcf129d5d5522
f11v <146761375+f11v@users.noreply.github.com>
2024-01-18 15:37:28 -0500

    Update GatewayMicrosApplicationTests.java

--- src/test/java/com/example/GatewayMicros/GatewayMicrosApplicationTests.java
+++ src/test/java/com/example/GatewayMicros/GatewayMicrosApplicationTests.java
@@ -3,7 +3,7 @@ package com.example.GatewayMicros;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class GatewayMicrosApplicationTests {
 
 	@Test
